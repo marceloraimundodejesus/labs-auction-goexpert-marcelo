dc094142ded417952713b02fbba7e4b3c7c71fc4
marceloraimundodejesus <marcelodejesus@live.com>
2025-09-26 11:14:32 -0300

    fix(auction): UpdateAuctionStatus usando _id string (UUID)

--- internal/infra/database/auction/update_auction.go
+++ internal/infra/database/auction/update_auction.go
@@ -6,16 +6,12 @@ import (
 	"fullcycle-auction_go/internal/entity/auction_entity"
 
 	"go.mongodb.org/mongo-driver/bson"
-	"go.mongodb.org/mongo-driver/bson/primitive"
 )
 
 func (ar *AuctionRepository) UpdateAuctionStatus(ctx context.Context, id string, status auction_entity.AuctionStatus) error {
-	oid, err := primitive.ObjectIDFromHex(id)
-	if err != nil {
-		return err
-	}
-	_, err = ar.Collection.UpdateByID(ctx, oid, bson.M{
-		"$set": bson.M{"status": status},
-	})
+	_, err := ar.Collection.UpdateOne(ctx,
+		bson.M{"_id": id},
+		bson.M{"$set": bson.M{"status": status}},
+	)
 	return err
 }
